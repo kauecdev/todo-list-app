0cda7cb29c0ce7d0539047fd735dafcaf8c5c368
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-05-12 01:26:35 -0300

    Added lucide icons package. Added input and button to add new todo on page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { CirclePlus } from 'lucide-react'
+
 import { Header } from './components/Header'
+import { Input } from './components/Input'
+
+import styles from './App.module.css'
 
 function App() {
   return (
     <main>
       <Header />
+
+      <section className={styles.container}>
+        <div className={styles.todoInputContainer}>
+          <Input />
+          <button>
+            Criar <CirclePlus size={16} />
+          </button>
+        </div>
+      </section>
     </main>
   )
 }
